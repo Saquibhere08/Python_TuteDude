1768ab2312d85dc8377dd906e899a874902857ee
Saquib Bin Halim <halimsaquib99@gmail.com>
2025-08-05 22:41:19 +0530

    Add files via upload

---
+++ mathematical operations.py
@@ -0,0 +1,6 @@
+num1=49**(1/2)
+print(num1)
+num2=9/1-1
+print(num2)
+num3=784.22%34
+print(num3)
---
+++ strings.py
@@ -0,0 +1,13 @@
+#Method 1
+print('Hello World')
+
+#comments single-line
+"""
+This is a multi line comment
+"""
+#Method 2
+print("Python\'s world")
+
+
+#Method 3
+print("Python's"'world')

8c72bbf0e053f20555988256f21ee09fff205dd5
Saquib Bin Halim <halimsaquib99@gmail.com>
2025-08-07 21:24:28 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,16 @@
+a=100
+print(a)
+b=200
+print(b)
+c=300
+print(c)
+d=a+b+c #addition
+print(d)
+
+u="python" #We can also store string values
+print(u)
+
+#Hello World
+a1="Hello"
+a2="World!"
+print(a1+a2)

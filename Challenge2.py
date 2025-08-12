a82705b1fd552ebb5ad0e0178523846c50efd90d
Saquib Bin Halim <halimsaquib99@gmail.com>
2025-08-12 19:27:10 +0530

    Add files via upload

---
+++ Challenge2.py
@@ -0,0 +1,10 @@
+#Simple Interest Calculator
+p=input('P: ')
+n=input('N: ')
+r=input('R: ')
+
+p=int(p)
+n=int(n)
+r=int(r)
+si=(n*p*r)/100
+print("Simple Interest is: ",si)
---
+++ challenge1.py
@@ -0,0 +1,12 @@
+#Celcius ----> farenheit
+
+c1=input('Celcius: ')
+c1=float(c1)
+f1=(c1* 9/5)+32
+print('Farenheit: ',f1)
+
+#Farenheit ----> celsius
+f2=input('Farenheit: ')
+f2=float(f2)
+c2=(f2-32) *5/9
+print('Celcius: ',c2)

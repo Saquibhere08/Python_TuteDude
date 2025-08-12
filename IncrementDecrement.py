71a72cd15f3df6799f8ca4cdef8a12e6843657e9
Saquib Bin Halim <halimsaquib99@gmail.com>
2025-08-12 14:16:36 +0530

    Add files via upload

---
+++ IncrementDecrement.py
@@ -0,0 +1,29 @@
+#Inrement
+#Method 1
+a=1
+a=a+1
+print(a)
+
+#Method 2
+b=2
+b+=1
+print(b)
+
+#Decrement
+#Method 1
+c=5
+c-=2
+print(c)
+
+#Random operations
+d=10
+d*=2
+print(d)
+
+e=6
+e/=2
+print(e)
+
+z="thank"
+z+="you!"
+print(z)

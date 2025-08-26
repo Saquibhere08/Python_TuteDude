e686c994dcfbdc16ca8b11ca97e1999691bcb7b4
Saquib Bin Halim <halimsaquib99@gmail.com>
2025-08-26 14:23:20 +0530

    Add files via upload

---
+++ functions_arguments2.py
@@ -0,0 +1,12 @@
+# 2 numbers, if you add these =7
+# Square of the number
+#1 function call to be used.
+
+def add(x,y):
+    return x+y
+def sqaure(z):
+    return z*z
+
+result=sqaure(add(3,4))
+print(result)
+

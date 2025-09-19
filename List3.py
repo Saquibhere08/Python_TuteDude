728f6d23912a1809dbbab6c788e5c0db5f0a438e
Saquib Bin Halim <halimsaquib99@gmail.com>
2025-09-19 21:34:24 +0530

    Add files via upload

---
+++ List3.py
@@ -0,0 +1,12 @@
+#List Operations 1
+a=['disco',0,1,2]
+a.append(540) #we can  add only single value
+print(a)
+a.extend([10,1]) #We can add more than one value
+print(a)
+
+del a[0] #remove a value
+print(a)
+
+a.clear #Used to clear all the data present in the list
+print(a) 
---
+++ List4.py
@@ -0,0 +1,15 @@
+a=[0,1,2,57,90,88]
+a.pop(2) #remove data from the list
+print(a)
+
+a.insert(2,'Saquib')
+print(a)
+
+#a.sort() #Used to sort values accordingly
+print(a)
+
+a.reverse() #Used to reverse the entire list of operations
+print(a)
+
+print(a.index(57)) #Finds the index number of the list
+print(len(a)) #calculates the total values of the list

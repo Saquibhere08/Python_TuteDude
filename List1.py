ce90b4e908fbf7d10b8c0bda4256f4249e904490
Saquib Bin Halim <halimsaquib99@gmail.com>
2025-09-19 21:10:39 +0530

    Add files via upload

---
+++ List1.py
@@ -0,0 +1,6 @@
+#List
+number=[1,2,3,4,5]
+print(number[2])
+
+mix=['Peter','Saquib','Nazeeb','Shubham']
+print(mix[2])
---
+++ List2.py
@@ -0,0 +1,13 @@
+list1=['Mike',10,1,1980]
+list2=['pop',1]
+print(list1 +list2) #adding both the seprate  list into one list
+
+a=['disco',0,1,2]
+a[0]='Hardwork' #replace the content
+print(a)
+#Lists are mutable
+
+game=['football','volleyball','chess']
+print('cricket' in game)
+#Checking weather the content is available on the list or not
+#It will provide only Boolean value i.e. True or False

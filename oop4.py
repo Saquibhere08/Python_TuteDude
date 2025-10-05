125008fa5c93e9e46ff8ba88d53f04965bbe44af
Saquib Bin Halim <halimsaquib99@gmail.com>
2025-10-05 12:12:28 +0530

    Add files via upload

---
+++ oop4.py
@@ -0,0 +1,19 @@
+#greet a person name using function
+
+def name(name):
+    print("HI",name)
+
+n=name("Saquib")
+n=name("Nazeeb")
+
+
+class name:
+    x=0
+    name=" "
+
+    def __init__(self,z):
+        self.name=z
+        print("hi",z)
+
+n=name("Sam")
+n=name("Jim")

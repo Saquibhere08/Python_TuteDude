83f8cd5e1f2c1b74ff4122d27a3196c3c7af29d8
Saquib Bin Halim <halimsaquib99@gmail.com>
2025-11-05 22:41:32 +0530

    Add files via upload

---
+++ ErrorException2.py
@@ -0,0 +1,8 @@
+"""
+Exception: An exception is an event that occurs during the execution of a program that disrupts the normal flow of instructions. 
+In Python, exceptions are handled using try and except blocks.
+"""
+print(10/0) #Run time Error
+
+x=100
+result=x+y
